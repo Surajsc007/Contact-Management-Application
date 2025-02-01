8f9e0d86bf5e31ae508e5ea704c9572a6ecc4b96
Suraj Chauhan <85945160+Surajsc007@users.noreply.github.com>
2025-02-01 14:46:24 +0530

    Update ContactService.js

--- Contact-App/src/api/ContactService.js
+++ Contact-App/src/api/ContactService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://contact-management-api-production.up.railway.app";
 
 export async function saveContact(contact) {
   return await axios.post(`${API_URL}/contacts`, contact);
